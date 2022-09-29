5a80340ece1ad5e21492e2960b540a16d9c4faee
Jose Luis Gonzalez <puchosoft@gmail.com>
2022-09-28 22:30:12 -0300

    Archivos de la clase#17

---
+++ clase#17/js/index.js
@@ -0,0 +1,30 @@
+/*	Algoritmo:
+	----------
+	Secuencia (lista)
+	de instrucciones (ordenes)
+	ordenada (respeta un orden de ejecucion)
+	finita (tiene principio y final)
+	con una meta o proposito.
+*/
+
+/*
+	Variable:
+	---------
+	Contenedor de datos
+	Tiene un nombre (no pueden iniciar con numeros, ni contener espacios)
+	Los nombres son sensibles a mayusculas ( Caballo <> caballo ).
+	Tiene un tipo de dato (numericos, texto, logicos).
+	Puede contener UN SOLO DATO a la vez.
+	Para operar con una variable se la llama por su NOMBRE
+*/
+
+var a = 10;
+var b = 15;
+
+var c = a * b;
+a = c + 8;
+
+console.log("El resultado es "+ a);
+alert("El programita ya terminó");
+
+
