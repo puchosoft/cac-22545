3841cf0f8dd8debad220e8d079dfd6e5e8401b46
Jose Luis Gonzalez <puchosoft@gmail.com>
2022-10-07 18:23:25 -0300

    Clase#20

--- clase_20/js/bucles.js
+++ clase_20/js/bucles.js
@@ -1,1 +1,81 @@
-// Bucles
+function parrafo(contenido) {
+    document.write('<p>'+ contenido + '</p>');
+}
+
+/*
+var n = 9;
+var i = 0;
+parrafo('Tabla de nº '+ n)
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+parrafo(n + ' x ' + i + ' = ' + n * i++);
+*/
+
+//  +--------+
+//  | Bucles |
+//  +--------+
+
+//  Bucle FOR (fijo o definido)
+var n = 9;
+parrafo('Tabla de nº '+ n);
+
+for(let i = 0 ; i < 10; i++) {
+    parrafo(n + ' x ' + i + ' = ' + n * i);
+    // i *= 2; NO SE RECOMIENDA ALTERAR LA VARIABLE DE CONTROL
+}
+
+//  Bucle WHILE (condicional al inicio)
+var tanque = 300;
+var agua = 234;
+var balde = 10.5;
+while(agua <= (tanque - balde)){
+    agua += balde; 
+    parrafo(agua + ' l')
+}
+parrafo('Ahora el tanque contiene '+ agua + ' litros.');
+
+//  Bucle DO .. WHILE (condicional al final)
+dist_pared = 4.7;
+paso = 0.96;
+do {
+    dist_pared -= paso;
+    parrafo('Distancia a la pared: '+ dist_pared);
+} while (dist_pared >= paso);
+
+
+//  BREAK (detener) y CONTINUE (saltar al proximo paso)
+// Uso de CONTINUE para hacer excepciones dentro de los bucles
+parrafo('El numero 17 me cae mal (y el 8 tambien).');
+for(let i = 1; i<21 ; i++){
+    if (i == 8 || i == 17)
+        continue;
+    parrafo('i = ' + i);
+}
+
+// Uso de BREAK para detener un bucle
+parrafo('Los numeros mayores a 13 me caen mal.');
+for(let i = 1; i<21 ; i++){
+    if (i > 13)
+        break;
+    parrafo('i = ' + i);
+}
+
+//  Bucles anidados
+
+// Este bucle repite 3 veces el mismo cuadradito
+for(let j=0; j<3 ; j++){
+    // Este bloque hace un cuadradito
+    document.write('-------<br>');
+    for(let i=0; i<5 ; i++){
+        document.write('|*****|<br>');
+    }
+    document.write('-------<br>');
+    document.write('<br>');
+}
--- clase_20/js/condicionales.js
+++ clase_20/js/condicionales.js
@@ -1,1 +1,174 @@
-// Condicionales
+function parrafo(contenido) {
+    document.write('<p>'+ contenido + '</p>');
+}
+
+//  +---------------+
+//  | Condicionales |
+//  +---------------+
+
+//  Condicional SIMPLE
+/*
+if (expresion logica) {
+    instruccion1;
+    instruccion2;
+    instruccion3;
+}
+*/
+parrafo('Condicional simple');
+a = 7;
+if (a > 5) {
+    parrafo(a + ' es mayor que 5');
+}
+
+//  Condicional COMPLETO
+/*
+if (expresion logica) {
+    instruccion1;
+    instruccion2;
+    instruccion3;
+}
+else {
+    instruccion4;
+    instruccion5;
+    instruccion6;
+}
+*/
+parrafo('Condicional completo');
+b = 3;
+if (b > 5) {
+    parrafo(b + ' es mayor que 5');
+}
+else {
+    parrafo(b + ' no es mayor que 5');
+}
+
+//  Deficion de variables VAR - LET - CONST
+parrafo('Contexto o SCOPE de una variable')
+var x = 18;
+parrafo(x);
+{
+    let y = 23;
+    var z = 53;
+    parrafo(x + ' ' + y);
+}
+parrafo(x + ' ' + z);
+
+const m = 17;
+{
+    // m = 21;
+    parrafo(m);
+}
+
+//  Operador condicional o ternario
+var luz = 0.8; // Varia entre 0 y 1
+// (expresion logica) ? valor_true : valor_false
+var luminosidad = (luz >= 0.5) ? "claro" : "oscuro";  // "claro" u "oscuro"
+parrafo(luminosidad);
+
+//  Operadores de asignacion
+// =    ->  asignacion estandar
+// +=   ->  x += 8;
+// -=   ->  z -= 2;
+// *=
+// /=
+// %=
+
+//  Condicionales ANIDADOS
+peso_max = 100;
+altura_min = 140;
+peso = 102;
+altura = 139;
+// La persona ¿puede ingresar a la atraccion?
+parrafo("¿Puede ingresar a la atraccion?");
+if (peso <= peso_max){
+    if (altura >= altura_min){
+        parrafo("Puede ingresar");
+    }
+    else {
+        parrafo("No puede ingresar por altura.");
+    }
+}
+else {
+    parrafo("No puede ingresar por peso.");
+}
+
+//  Condicionales en CASCADA
+var mayoria = 18;
+var jubilacion = 65;
+var edad = 10;
+
+if (edad < mayoria) {
+    parrafo("Menor de edad");
+}
+else {
+    if (edad < jubilacion){
+        parrafo("En edad laboral");
+    }
+    else {
+        parrafo("En edad jubilatoria");
+    }
+}
+
+//  Condicional ANIDADO simplificado con op. logicos
+peso_max = 100;
+altura_min = 140;
+peso = 102;
+altura = 139;
+// La persona ¿puede ingresar a la atraccion?
+parrafo("¿Puede ingresar a la atraccion?");
+
+if ((peso <= peso_max) && (altura >= altura_min)) {
+    parrafo("Puede ingresar");
+}
+else {
+    parrafo("No puede ingresar");
+}
+
+//  Condicional MULTIPLE - Simplificacion de CASCADAS
+// 1 = cafe
+// 2 = te
+// 3 = matecocido
+// 4 = chocolatada
+
+var opcion = 1;
+
+/*
+if (opcion == 1) {
+    parrafo('Sirvase su cafe');
+}
+else {
+    if (opcion == 2) {
+        parrafo('Sirvase su te');
+    }
+    else {
+        if (opcion == 3) {
+            parrafo('Sirvase su matecocido');
+        }
+        else {
+            if (opcion == 4) {
+                parrafo('Sirvase su chocolatada');
+            }
+            else {
+                parrafo('*** Opcion no valida ***');
+            }
+        }
+    }
+}
+*/
+
+switch (opcion) {
+    case 1:
+        parrafo('Sirvase su cafe');
+        break;
+    case 2:
+        parrafo('Sirvase su te');
+        break;
+    case 3:
+        parrafo('Sirvase su matecocido');
+        break;
+    case 4:
+        parrafo('Sirvase su chocolatada');
+        break;
+    default:
+        parrafo('*** Opcion no valida ***');
+}
