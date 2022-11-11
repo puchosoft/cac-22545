a32d0950d05f11fca97a539c98d2550a20e43bed
Jose Luis Gonzalez <puchosoft@gmail.com>
2022-11-11 15:12:40 -0300

    Clase#29

---
+++ clase_29/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        int x; // Declaracion de la variable entera 'x'
+
+        // Declaracion e inicializacion de la variable 'msj'
+        String msj = "¡Hola Mundo!";
+        char c = '$';
+
+        System.out.println(msj);
+    }
+}
